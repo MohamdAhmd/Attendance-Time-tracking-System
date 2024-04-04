2a3fe3e7a1cd2fdfcc5cb5a46152ca23e6cd70da
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-04-04 18:17:54 +0200

    add track CRUD

---
+++ Attendance-Time-tracking-System/Controllers/TracksController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Attendance_Time_tracking_System.Models;
+
+namespace Attendance_Time_tracking_System.Controllers
+{
+    public class TracksController : Controller
+    {
+        ITrackRepo trackRepo;
+        IProgramRepo programRepo;
+        public TracksController(ITrackRepo trackRepo, IProgramRepo programRepo)
+        {
+            this.trackRepo = trackRepo;
+            this.programRepo = programRepo;
+        }
+
+        // GET: Tracks
+        public IActionResult Index()
+        {
+            return View(trackRepo.GetAllTracks());
+        }
+
+        // GET: Tracks/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var track = trackRepo.GetTrackById(id.Value);
+            if (track == null)
+            {
+                return NotFound();
+            }
+            return View(track);
+        }
+
+        // GET: Tracks/Create
+        public IActionResult Create()
+        {
+            ViewData["SupervisorID"] = new SelectList(trackRepo.GetAllInstructors(), "Id", "Email");
+            ViewData["ProgramID"] = new SelectList(programRepo.GetAllPrograms(), "Id", "Name");
+            return View();
+        }
+
+        // POST: Tracks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Status,SupervisorID,ProgramID,Capacity")] Track track)
+        {
+            if (ModelState.IsValid)
+            {
+                trackRepo.AddTrack(track);
+                return RedirectToAction("index");
+            }
+            ViewData["SupervisorID"] = new SelectList(trackRepo.GetAllInstructors(), "Id", "Email");
+            ViewData["ProgramID"] = new SelectList(programRepo.GetAllPrograms(), "Id", "Name");
+            return View("create", track);
+        }
+
+        // GET: Tracks/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var track = trackRepo.GetTrackById(id.Value);
+            if (track == null)
+            {
+                return NotFound();
+            }
+            ViewData["SupervisorID"] = new SelectList(trackRepo.GetAllInstructors(), "Id", "Email");
+            ViewData["ProgramID"] = new SelectList(programRepo.GetAllPrograms(), "Id", "Name");
+            return View(track);
+        }
+
+        // POST: Tracks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Name,Status,SupervisorID,ProgramID,Capacity")] Track track)
+        {
+            if (id != track.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    trackRepo.UpdateTrack(track);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrackExists(track.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SupervisorID"] = new SelectList(trackRepo.GetAllInstructors(), "Id", "Email");
+            ViewData["ProgramID"] = new SelectList(programRepo.GetAllPrograms(), "Id", "Name");
+            return View(track);
+        }
+
+        // GET: Tracks/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var track = trackRepo.GetTrackById(id.Value);
+            if (track == null)
+            {
+                return NotFound();
+            }
+
+            return View(track);
+        }
+
+        // POST: Tracks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var track = trackRepo.GetTrackById(id);
+            if (track == null)
+            { 
+                return NotFound();
+            }
+
+            trackRepo.DeleteTrack(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrackExists(int id)
+        {
+            return trackRepo.GetTrackById(id) != null;
+        }
+    }
+}
--- Attendance-Time-tracking-System/Repos/TrackRepo.cs
+++ Attendance-Time-tracking-System/Repos/TrackRepo.cs
@@ -10,10 +10,38 @@ namespace Attendance_Time_tracking_System.Repos
             this.db = db;
         }
 
+        public void AddTrack(Track track)
+        {
+            db.Tracks.Add(track);
+            db.SaveChanges();
+        }
+
+        public void DeleteTrack(int id)
+        {
+            var track = db.Tracks.FirstOrDefault(t => t.Id == id);
+            db.Tracks.Remove(track);
+            db.SaveChanges();
+        }
+        public List<Instructor> GetAllInstructors()
+        {
+            return db.Instructors.ToList();
+        }
+
         public List<Track> GetAllTracks()
         {
-            var model = db.Tracks.Include(x=>x.InstructorNavigation).Include(x=>x.ProgramNavigation).ToList();
-            return model;
+            return db.Tracks.Include(t => t.InstructorNavigation).Include(t => t.ProgramNavigation).ToList();
+        }
+
+        public Track GetTrackById(int id)
+        {
+            return db.Tracks.Include(t => t.InstructorNavigation).Include(t => t.ProgramNavigation).FirstOrDefault(t => t.Id == id);
+        }
+
+        public void UpdateTrack(Track track)
+        {
+            db.Tracks.Update(track);
+            db.SaveChanges();
+
         }
     }
 }
