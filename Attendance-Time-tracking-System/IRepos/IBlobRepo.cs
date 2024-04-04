6311b9c2a2b49f47443044fcd8b016d2b1eff95e
abdellatief <abdellatiefhamed00@gmail.com>
2024-04-04 18:15:59 +0200

    adding blob repo

---
+++ Attendance-Time-tracking-System/IRepos/IBlobRepo.cs
@@ -0,0 +1,9 @@
+﻿namespace Attendance_Time_tracking_System.IRepos
+{
+    public interface IBlobRepo
+    {
+        public Task<string> AddingImage(IFormFile image);
+        public Task<bool> RemoveImage(string imagename);
+        public Task<string> UpdateImage(string oldimage, IFormFile image);
+    }
+}
---
+++ Attendance-Time-tracking-System/Repos/BlobRepo.cs
@@ -0,0 +1,81 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Attendance_Time_tracking_System.Repos
+{
+    public class BlobRepo : IBlobRepo
+    {
+
+        private readonly string _storageConnectionString;
+        private const string ContainerName = "imagecontainer";
+        private readonly BlobServiceClient blobServiceClient;
+        private readonly BlobContainerClient blobContainerClient;
+
+        public BlobRepo(IConfiguration configuration)
+        {
+            _storageConnectionString = configuration.GetConnectionString("AzureStorageConnectionString") ??
+                "DefaultEndpointsProtocol=https;AccountName=storageattendanceiti;AccountKey=05SsoPSpGGzU+90G09IlIhMW15yE6pZe9JTc3jdz/N9sYqTgHr8B2RogWGU6f2pMwXoaBUn/yQtS+AStsyfs4Q==;EndpointSuffix=core.windows.net"; 
+            blobServiceClient = new BlobServiceClient(_storageConnectionString);
+            blobContainerClient = blobServiceClient.GetBlobContainerClient(ContainerName);
+        }
+
+        public async Task<string> AddingImage(IFormFile image)
+        {
+            if (image == null)
+            {
+                return null;
+            }
+            try
+            {
+                await blobContainerClient.CreateIfNotExistsAsync();
+                var blobName = $"{Guid.NewGuid()}{Path.GetExtension(image.FileName)}";
+                var blobClient = blobContainerClient.GetBlobClient(blobName);
+                await blobClient.UploadAsync(image.OpenReadStream(), true);
+
+                var imageUrl = blobClient.Uri.ToString();
+                return imageUrl;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> RemoveImage(string imagename)
+        {
+            var uri = new Uri(imagename);
+            var blobname = Path.GetFileName(uri.LocalPath);
+            var blobServiceClient = new BlobServiceClient(_storageConnectionString);
+
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(ContainerName);
+
+            // Get the blob client
+            var blobClient = blobContainerClient.GetBlobClient(blobname);
+
+            // Delete the blob
+            var response = await blobClient.DeleteIfExistsAsync();
+
+            if (response != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<string> UpdateImage(string oldimage, IFormFile image)
+        {
+            try
+            {
+                if (await RemoveImage(oldimage))
+                {
+                    return await AddingImage(image);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
