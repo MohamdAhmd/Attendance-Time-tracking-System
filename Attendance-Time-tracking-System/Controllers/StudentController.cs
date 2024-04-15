523582480ebdcff178e2b5a2592d165cfe9f07b5
Nada Atef <nadaatefelsheshtawy@gmail.com>
2024-04-15 14:31:03 +0200

    showAttendanceForStudent

---
+++ Attendance-Time-tracking-System/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Attendance_Time_tracking_System.Migrations;
+using Attendance_Time_tracking_System.Models;
+using Attendance_Time_tracking_System.Repos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Attendance_Time_tracking_System.Controllers
+{
+    [Authorize(Roles = "Student")]
+    public class StudentController : Controller
+    {
+        readonly dbContext db;
+       
+        public StudentController(dbContext _db )
+        {
+            db= _db;
+        }
+        public IActionResult ShowAttendance()
+        {
+            var userIdClaim = HttpContext.User.FindFirst("UserId");
+            int id = int.Parse(userIdClaim.Value);
+
+            var model = db.Attends.Where(a=>a.UserId==id )       
+                        .Include(a => a.UserNavigation)
+                        .Include(a => a.DaysNavigation)  
+                        .ToList();
+                        ;
+
+            return View(model);       
+
+        }
+    }
+}
