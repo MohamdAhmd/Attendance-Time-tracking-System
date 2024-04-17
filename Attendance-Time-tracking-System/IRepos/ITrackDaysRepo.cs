a4004a1ae7ff52f0b0c2aedc7ca7b543464933c6
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-04-17 17:09:12 +0200

    Done

--- Attendance-Time-tracking-System/IRepos/ITrackDaysRepo.cs
+++ Attendance-Time-tracking-System/IRepos/ITrackDaysRepo.cs
@@ -3,9 +3,12 @@
     public interface ITrackDaysRepo
     {
         public IEnumerable<TrackDays> GetAllTrackDays();
+        //get supervisor track days
+        public IEnumerable<TrackDays> GetTrackDaysBySupervisorId(int id);
+
         public TrackDays GetTrackDayById(int dayId, int trackId);
         public void AddTrackDay(TrackDays trackDays);
-        public void UpdateTrackDay(TrackDays trackDays);
+        public void UpdateTrackDay(TrackDays trackDays,int? dayId);
         public void DeleteTrackDay(int id);
         
     }
